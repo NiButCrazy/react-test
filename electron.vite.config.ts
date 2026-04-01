35eaa923c85455ca64fa137b04b04d3ec391a53e
NiButCrazy <3104955349@qq.com>
2026-04-01 11:57:57 +0800

    fix: 部分设置

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,18 +1,55 @@
-import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
+// noinspection JSUnusedGlobalSymbols
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
+    plugins: [ externalizeDepsPlugin() ],
+    publicDir: './static',
+    build: {
+      outDir: '.out/main',
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/main/main.ts')
+        }
+      }
+    },
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@static': resolve(__dirname, './static')
+      }
+    }
+  },
+  preload: {
+    plugins: [ externalizeDepsPlugin() ],
+    publicDir: './static',
+    build: {
+      outDir: '.out/preload',
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/preload/preload.ts')
+        }
+      }
+    }
+  },
+  renderer: {
+    build: {
+      outDir: '.out/renderer'
+    },
+    resolve: {
+      alias: {
+        '@renderer': resolve(__dirname, './src/renderer/src'),
+        '@components': resolve(__dirname, './src/renderer/src/components'),
+        '@utils': resolve(__dirname, './src/renderer/src/utils'),
+        '@global': resolve(__dirname, './src/renderer/src/global')
+      }
+    },
+    css: {
+      devSourcemap: true,
+      modules: {
+        localsConvention: 'dashesOnly',
+        generateScopedName: '[local]-[hash:5]'
       }
     },
     plugins: [
